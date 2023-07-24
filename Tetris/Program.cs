aa11540929e515a2f860eca5882a758d80a41199
Vadin Maryushkin <maryushkin.vadim@gmail.com>
2023-07-24 13:08:37 +0300

    Добавил код с выводом 2 символов

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿using System;
+
+namespace Tetris
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{ 
+			Console.SetWindowSize(40, 30);
+			Console.SetBufferSize(40,30);
+
+			int x1 = 2;
+			int y1 = 3;
+			char c1 = '*';
+
+			Drow(x1, y1, c1);
+
+            int x2 = 3;
+            int y2 = 4;
+            char c2 = '#';
+
+            Drow(x2, y2, c2);
+
+
+
+            Console.ReadLine();
+        }
+
+		static void Drow(int x, int y, char	c)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(c);
+		}
+
+    }
+}
